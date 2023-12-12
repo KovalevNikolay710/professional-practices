7bab4893e2907f21ec76f505b6d3b42a7d05f573
NikolayKovalev <nkovalev602@gmail.com>
2023-12-12 15:32:23 +0300

    Вроде всё

--- Stack/Brackets.py
+++ Stack/Brackets.py
@@ -11,20 +11,3 @@
 #is_valid("{[]}")            # Вернет True
 #is_valid("{{[()()]}}")      # Вернет True
 
-def is_valid(s):
-    stack = []
-    brackets = {')': '(', '}': '{', ']': '['}
-
-    for char in s:
-        if char in brackets.values():
-            stack.append(char)
-        elif char in brackets.keys():
-            if not stack or stack.pop() != brackets[char]:
-                return False
-        else:
-            return False
-
-    return not stack
-
-print(is_valid("{}"))
-
--- Stack/Palindrome.py
+++ Stack/Palindrome.py
@@ -1,30 +1,6 @@
 #Условие:
-#Дана строка, содержащая только символы '(', ')', '{', '}', '[' и ']'. Проверьте, является ли расстановка скобок в этой строке правильной.
-#
-#Примеры:
-#python
-#Copy code
-#is_valid("()")              # Вернет True
-#is_valid("()[]{}")          # Вернет True
-#is_valid("(]")              # Вернет False
-#is_valid("([)]")            # Вернет False
-#is_valid("{[]}")            # Вернет True
-#is_valid("{{[()()]}}")      # Вернет True
-
-def is_valid(s):
-    stack = []
-    brackets = {')': '(', '}': '{', ']': '['}
-
-    for char in s:
-        if char in brackets.values():
-            stack.append(char)
-        elif char in brackets.keys():
-            if not stack or stack.pop() != brackets[char]:
-                return False
-        else:
-            return False
-
-    return not stack
-
-print(is_valid("{}"))
+#Дана строка, проверьте, является ли она палиндромом (читается одинаково слева направо и справа налево). Игнорируйте пробелы, регистр и пунктуацию.
+#Пример:
+#is_palindrome("A man, a plan, a canal, Panama")  # Вернет True
+#is_palindrome("race a car")                      # Вернет False
 
